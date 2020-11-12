0b46bb15e3a51890241a874a9b33ca05af8b4d26
Dmitriy <shalberkind@mail.ru>
2020-11-13 00:03:05 +0300

    Разметка главной страницы

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <div className="container pt-4"></div>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/profile:name" component={Profile} />
-      </Switch>
+      <div className="container pt-4">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/profile:name" component={Profile} />
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Card } from "../components/Card";
+import { Search } from "../components/Search";
 
 export const Home = () => {
+  const cards = new Array(15).fill("").map((_, idx) => idx);
+  console.log(cards);
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <Search />
+
+      <div className="row">
+        {cards.map((card) => {
+          return (
+            <div className="col-sm-4 mb-4" key={card}>
+              <Card />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };

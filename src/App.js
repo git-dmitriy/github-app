c36daa165777dbcc4f00eacf957183851133d8f6
Dmitriy <shalberkind@mail.ru>
2020-11-12 21:18:20 +0300

    Первоначальная настройка проекта

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container pt-4">
+      <h1>Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;

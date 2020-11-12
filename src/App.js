0de65ca48e43dbddbbb79a34d798a87deab38c55
Dmitriy <shalberkind@mail.ru>
2020-11-12 23:41:30 +0300

    Настроен Роутинг ссылок

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
 
 function App() {
   return (
-    <div className="container pt-4">
-      <h1>Hello world!</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container pt-4"></div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profile:name" component={Profile} />
+      </Switch>
+    </Router>
   );
 }
 

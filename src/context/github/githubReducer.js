37e300fb32bfed40e84e45f81f9da2e231710718
Dmitriy <shalberkind@mail.ru>
2020-11-13 16:44:54 +0300

    Reducer для связи с github

---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,33 @@
+import {
+  CLEAR_USERS,
+  GET_REPOSITORIES,
+  GET_USERS,
+  SEARCH_USERS,
+  SET_LOADING,
+} from "../types";
+
+const handlers = {
+  [SEARCH_USERS]: (state, { payload }) => ({
+    ...state,
+    users: payload,
+    loading: false,
+  }),
+  [GET_REPOSITORIES]: (state, { payload }) => ({
+    ...state,
+    repositories: payload,
+    loadig: false,
+  }),
+  [GET_USERS]: (state, { payload }) => ({
+    ...state,
+    user: payload,
+    loadig: false,
+  }),
+  [SET_LOADING]: (state) => ({ ...state, loading: true }),
+  [CLEAR_USERS]: (state) => ({ ...state, users: [] }),
+  DEFAULT: (state) => state,
+};
+
+export const githubReducer = (state, action) => {
+  const handler = handlers[action.type] || handlers.DEFAULT;
+  return handler(state, action);
+};

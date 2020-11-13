d2193ce45e55c83209c75f3458ff093fb54b101c
Dmitriy <shalberkind@mail.ru>
2020-11-13 15:58:17 +0300

    Функционал компонента поиска
    Получение значения ввода пользователя,
    submit по клавише Enter

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AlertContext } from "../context/alert/alertContext";
 
 export const Search = () => {
+  const [value, setValue] = useState("");
+  const { show } = useContext(AlertContext);
+
+  const onSubmit = (event) => {
+    if (event.key !== "Enter") {
+      return;
+    }
+    if (value.trim()) {
+      console.log(`make requist: ${value}`);
+    } else {
+      show("Введите данные пользователя");
+    }
+  };
+
   return (
     <div className="form-group">
       <input
         type="text"
         className="form-control"
         placeholder="Введите user name пользователя"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        onKeyPress={onSubmit}
       />
     </div>
   );

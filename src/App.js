16391228fdda4482f6213987a7be438e2e3fdf86
Dmitriy <shalberkind@mail.ru>
2020-11-13 11:55:54 +0300

    Копнент для передачи состояния компонента Alert

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Alert } from "./components/Alert";
 import { Navbar } from "./components/Navbar";
+import alertState from "./context/alert/alertState";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="container pt-4">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/profile:name" component={Profile} />
-        </Switch>
-      </div>
-    </Router>
+    <alertState>
+      <Router>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert alert={{ text: "test-alert" }} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/profile:name" component={Profile} />
+          </Switch>
+        </div>
+      </Router>
+    </alertState>
   );
 }
 

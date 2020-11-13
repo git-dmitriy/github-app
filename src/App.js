406bbb05570849241826527dccf96d91e7118af3
Dmitriy <shalberkind@mail.ru>
2020-11-13 16:44:08 +0300

    Передача контекста для связи с github

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Alert } from "./components/Alert";
 import { Navbar } from "./components/Navbar";
 import { AlertState } from "./context/alert/alertState";
+import { GithubContext } from "./context/github/githubContext";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
 
 function App() {
   return (
-    <AlertState>
-      <Router>
-        <Navbar />
-        <div className="container pt-4">
-          <Alert alert={{ text: "test-alert" }} />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/profile:name" component={Profile} />
-          </Switch>
-        </div>
-      </Router>
-    </AlertState>
+    <GithubContext>
+      <AlertState>
+        <Router>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{ text: "test-alert" }} />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/profile:name" component={Profile} />
+            </Switch>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubContext>
   );
 }
 

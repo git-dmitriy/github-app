145c7917cde3f10468f7508392cb096fde704a5a
Dmitriy <shalberkind@mail.ru>
2020-11-14 21:04:22 +0300

    Настрока запросов

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -18,6 +18,10 @@ export const GithubState = ({ children }) => {
     repositories: [],
   };
 
+  const withCreds = (url) => {
+    return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+  };
+
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
   const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
@@ -30,7 +34,7 @@ export const GithubState = ({ children }) => {
     setLoading();
 
     const response = await axios.get(
-      `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+      withCreds(`https://api.github.com/search/users?q=${value}&`)
     );
 
     dispatch({
@@ -41,16 +45,24 @@ export const GithubState = ({ children }) => {
 
   const getUser = async (name) => {
     setLoading();
-    // ...
+
+    const response = await axios.get(
+      withCreds(`https://api.github.com/users/${name}?`)
+    );
+
     dispatch({
       type: GET_USERS,
-      payload: {},
+      payload: response.data,
     });
   };
 
   const getRepositories = async (name) => {
     setLoading();
-    // ...
+
+    const response = await axios.get(
+      withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+    );
+
     dispatch({
       type: GET_REPOSITORIES,
       payload: [],

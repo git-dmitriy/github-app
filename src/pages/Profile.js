586d26f67f619d071f4b7d46ef25bf7e637140bf
Dmitriy <shalberkind@mail.ru>
2020-11-15 00:36:16 +0300

    Вывод информации о пользователе

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,100 @@
-import React from "react";
+import React, { Fragment, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { GithubContext } from "../context/github/githubContext";
 
-export const Profile = () => {
+export const Profile = ({ match }) => {
+  const { getUser, getRepositories, loading, user, repositories } = useContext(
+    GithubContext
+  );
+  const values = useContext(GithubContext);
+  console.log(values);
+
+  const urlName = match.params.name;
+
+  useEffect(() => {
+    getUser(urlName);
+    getRepositories(urlName);
+    // eslint-disable-next-line
+  }, []);
+
+  if (loading) {
+    return <p className="text-center">Загрузка...</p>;
+  }
+
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+  } = user;
+
   return (
     <div>
-      <h1>Profile page</h1>
+      <Link to="/" className="btn btn-link">
+        На главную
+      </Link>
+      <div className="card mb-4">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-3 text-center">
+              <img src={avatar_url} alt={name} style={{ width: "150px" }} />
+              <h1>Name</h1>
+              {location && <p>Местоположение: {location}</p>}
+            </div>
+            <div className="col">
+              {bio && (
+                <Fragment>
+                  <h3>BIO</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              )}
+              <a
+                href={html_url}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-dark"
+              >
+                Открыть профиль
+              </a>
+              <ul>
+                {login && (
+                  <li>
+                    {" "}
+                    <strong>Пользователь: </strong> {login}{" "}
+                  </li>
+                )}
+                {company && (
+                  <li>
+                    {" "}
+                    <strong>Компания: </strong> {company}{" "}
+                  </li>
+                )}
+                {blog && (
+                  <li>
+                    {" "}
+                    <strong>Вебсайт: </strong> {blog}{" "}
+                  </li>
+                )}
+              </ul>
+              <div className="badge badge-primary">Подписчики: {followers}</div>
+              <div className="badge badge-success">Подписан: {following}</div>
+              <div className="badge badge-info">
+                Репозитории: {public_repos}
+              </div>
+              <div className="badge badge-dark">Gists: {public_gists}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {repositories.join()}
     </div>
   );
 };

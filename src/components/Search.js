968a0eaa07f043162b50b470d9651661ecf15d75
Dmitriy <shalberkind@mail.ru>
2020-11-14 15:01:11 +0300

    Получение данных с github

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,18 @@
 import React, { useContext, useState } from "react";
 import { AlertContext } from "../context/alert/alertContext";
+import { GithubContext } from "../context/github/githubContext";
 
 export const Search = () => {
   const [value, setValue] = useState("");
   const { show } = useContext(AlertContext);
+  const github = useContext(GithubContext);
 
   const onSubmit = (event) => {
     if (event.key !== "Enter") {
       return;
     }
     if (value.trim()) {
-      console.log(`make requist: ${value}`);
+      github.search(value.trim());
     } else {
       show("Введите данные пользователя");
     }
--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,20 +1,82 @@
-import React, {useReducer} from 'react'
-import { GithubContext } from './githubContext'
-import { githubReducer } from './githubReducer'
+import axios from "axios";
+import React, { useReducer } from "react";
+import {
+  CLEAR_USERS,
+  GET_REPOSITORIES,
+  GET_USERS,
+  SEARCH_USERS,
+  SET_LOADING,
+} from "../types";
+import { GithubContext } from "./githubContext";
+import { githubReducer } from "./githubReducer";
 
-export const GithubState = ({children}) => {
+export const GithubState = ({ children }) => {
   const initialState = {
     user: {},
     users: [],
     loading: false,
     repositories: [],
-  }
+  };
 
-  const [state, dispatch] = useReducer(githubReducer, initialState)
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
-  return(
-    <GithubContext.Provider value={{}}>
+  console.log(CLIENT_ID);
+  console.log(CLIENT_SECRET);
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const search = async (value) => {
+    setLoading();
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: response.data.items,
+    });
+  };
+
+  const getUser = async (name) => {
+    setLoading();
+    // ...
+    dispatch({
+      type: GET_USERS,
+      payload: {},
+    });
+  };
+
+  const getRepositories = async (name) => {
+    setLoading();
+    // ...
+    dispatch({
+      type: GET_REPOSITORIES,
+      payload: [],
+    });
+  };
+
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  const { user, users, repos, loading } = state;
+
+  return (
+    <GithubContext.Provider
+      value={{
+        search,
+        getUser,
+        getRepositories,
+        clearUsers,
+        setLoading,
+        user,
+        users,
+        repos,
+        loading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
-  )
-} 
+  );
+};

8533b1571a0b61599ce6e884a6f229a74fa48eed
Dmitriy <shalberkind@mail.ru>
2020-11-14 23:21:25 +0300

    Исправлена опечатка

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export const Card = ({ user }) => (
     <div className="card-body">
       <img src={user.avatar_url} alt={user.login} className="card-img-top" />
       <h5 className="card-title">{user.login}</h5>
-      <Link to={`/propfile/${user.login}`} className="btn btn-primary">
+      <Link to={`/profile/${user.login}`} className="btn btn-primary">
         Открыть
       </Link>
     </div>

2a33fed6d72b859f93cfc852d430f6952d8da704
Dmitriy <shalberkind@mail.ru>
2021-01-27 21:14:57 +0300

    Create app description

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,15 +3,12 @@ import React from "react";
 export const About = () => {
   return (
     <div className="jumbotron">
-      <h1 className="display-4">Hello, world!</h1>
-      <p className="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to eatured content or information.
-      </p>
+      <h1 className="display-4">Github Клиент</h1>
+      <p className="lead">Приложение созданное для знакомства с github api.</p>
       <hr className="my-4" />
       <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
+        Позволяет находить пользователей, получать краткую информацию и
+        открывать первые десять репозиториев пользователя.
       </p>
       <a className="btn btn-primary btn-lg" href="#" role="button">
         Learn more
